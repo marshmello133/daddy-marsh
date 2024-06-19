a27d1cdcc11b1fd07289265d72649e823bc544ff
marshmello133 <58664226+marshmello133@users.noreply.github.com>
2024-06-19 12:45:03 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 // script.js
 document.getElementById('reveal-button').addEventListener('click', function() {
-    document.getElementById('popup').classList.add('visible');
+    document.getElementById('popup1').classList.add('visible');
+});
+
+document.getElementById('reveal-girlfriend').addEventListener('click', function() {
+    document.getElementById('popup1').classList.remove('visible');
+    document.getElementById('popup2').classList.add('visible');
+    document.getElementById('song').play();
 });
